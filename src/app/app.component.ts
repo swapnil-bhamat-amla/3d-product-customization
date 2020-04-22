e0b1f41982faeb35fe50ba0979ec32673aa639c1
Swapnil Bhamat <bhamatswapnil@gmail.com>
2020-04-22 18:04:56 +0530

    Feat: Implemented textbox component panel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface ITextProp {
+  fill: string;
+  text: string;
+  fontSize: number;
+  fontWeight: boolean;
+  fontStyle: boolean;
+  fontFamily: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'get-image-admin-less';
+export class AppComponent implements OnInit {
+  props: ITextProp;
+
+  constructor() {
+    this.props = {
+      fill: '#000000',
+      text: 'sample text',
+      fontSize: 26,
+      fontWeight: false,
+      fontStyle: false,
+      fontFamily: 'helvetica',
+    };
+  }
+
+  ngOnInit() {}
+
+  setText() {
+    this.setActiveStyle('text', this.props.text);
+  }
+
+  setFontSize() {
+    this.setActiveStyle('fontSize', +this.props.fontSize);
+  }
+
+  setFontStyle() {
+    this.props.fontStyle = !this.props.fontStyle;
+    this.setActiveStyle('fontStyle', this.props.fontStyle ? 'italic' : '');
+  }
+
+  setBold() {
+    this.props.fontWeight = !this.props.fontWeight;
+    this.setActiveStyle('fontWeight', this.props.fontWeight ? 'bold' : '');
+  }
+
+  setFontFamily() {
+    this.setActiveStyle('fontFamily', this.props.fontFamily);
+  }
+
+  setFill() {
+    this.setActiveStyle('fill', this.props.fill);
+  }
+
+  setActiveStyle(styleName: string, value: string | number) {
+    console.log(styleName, value);
+  }
+
+  getImgPolaroid(event: any) {
+    let el = event.target;
+    console.log(el.src);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { ColorPickerModule } from 'ngx-color-picker';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, ColorPickerModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
