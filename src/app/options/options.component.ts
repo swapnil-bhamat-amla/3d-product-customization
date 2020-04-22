0bc5b5fc48290382f0b32dfcb3d1b971ee9cf91f
Swapnil Bhamat <bhamatswapnil@gmail.com>
2020-04-22 23:56:32 +0530

    Feat: Implemented product options panel

--- src/app/options/options.component.ts
+++ src/app/options/options.component.ts
@@ -1,12 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IProduct {
+  sku: string;
+  name: string;
+  hex: string;
+}
+
 @Component({
   selector: 'app-options',
   templateUrl: './options.component.html',
   styleUrls: ['./options.component.css'],
 })
 export class OptionsComponent implements OnInit {
-  constructor() {}
+  selectedSKU = '#fff';
+  products: IProduct[] = [];
+  constructor() {
+    this.products = [
+      {
+        sku: 'Product_V_Black',
+        name: 'Black',
+        hex: '#000000',
+      },
+      {
+        sku: 'Product_V_Blue',
+        name: 'Blue',
+        hex: '#0000ff',
+      },
+      {
+        sku: 'Product_V_Red',
+        name: 'Red',
+        hex: '#ff0000',
+      },
+    ];
+  }
 
   ngOnInit(): void {}
+
+  colorChangedHnd(sku: string) {
+    this.selectedSKU = sku;
+    console.log(sku);
+  }
 }
